83cdd4f8592888447e2e5120c12fb6dd29bff381
Tuna Han Bulanık <tunahanbulanik@outlook.com>
2024-08-19 21:21:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace W5P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz ve karesini alacağız");
+                string userInput = Console.ReadLine();
+
+                int makeNum = int.Parse(userInput);
+
+                double squareNum = Math.Pow(makeNum, 2);  // Square a number
+
+                Console.WriteLine($"Girdiğiniz sayının karesi {squareNum}");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("WRONG INPUT AGACIM");
+            }
+        }
+    }
+}
